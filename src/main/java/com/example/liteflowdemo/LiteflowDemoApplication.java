2072b2686f5f46efbe52ac9fc798d1e09f934ea1
Huang-Tony <tonyh2027@student.cis.edu.hk>
2025-05-14 15:55:20 +0800

    used Nashorn engine, allowing user to evaluate dynamic conditions on the fly

--- src/main/java/com/example/liteflowdemo/LiteflowDemoApplication.java
+++ src/main/java/com/example/liteflowdemo/LiteflowDemoApplication.java
@@ -2,9 +2,10 @@ package com.example.liteflowdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import com.yomahub.liteflow.spring.annotation.EnableLiteflow;
+;
 
 @SpringBootApplication
+// @EnableLiteflow
 public class LiteflowDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(LiteflowDemoApplication.class, args);
--- src/main/java/com/example/liteflowdemo/components/DynamicConditionComponent.java
+++ src/main/java/com/example/liteflowdemo/components/DynamicConditionComponent.java
@@ -8,37 +8,6 @@ import org.springframework.stereotype.Component;
 public class DynamicConditionComponent extends NodeComponent {
     @Override
     public void process() throws Exception {
-        NumberCheckContext context = this.getContextBean(NumberCheckContext.class);
-        double number = context.getInputNumber();
-        String condition = context.getCondition();
-        
-        boolean result = evaluateCondition(number, condition);
-        String resultStr = number + " " + condition + " is " + result;
-        
-        context.addExecutionStep("Condition evaluated: " + condition);
-        context.setFinalResult(resultStr);
-    }
-    
-    private boolean evaluateCondition(double number, String condition) {
-        if (condition.startsWith(">")) {
-            double value = Double.parseDouble(condition.substring(1));
-            return number > value;
-        } else if (condition.startsWith("<")) {
-            double value = Double.parseDouble(condition.substring(1));
-            return number < value;
-        } else if (condition.startsWith(">=")) {
-            double value = Double.parseDouble(condition.substring(2));
-            return number >= value;
-        } else if (condition.startsWith("<=")) {
-            double value = Double.parseDouble(condition.substring(2));
-            return number <= value;
-        } else if (condition.startsWith("==")) {
-            double value = Double.parseDouble(condition.substring(2));
-            return number == value;
-        } else if (condition.startsWith("!=")) {
-            double value = Double.parseDouble(condition.substring(2));
-            return number != value;
-        }
-        throw new IllegalArgumentException("Invalid condition: " + condition);
+//using script
     }
 }
